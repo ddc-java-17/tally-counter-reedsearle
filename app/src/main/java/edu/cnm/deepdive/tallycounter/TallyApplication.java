685f38d8054721a483dcdc820f17ede8f0739641
Reed Searle <csearle@cnm.edu>
2024-02-05 11:32:45 -0700

    Initial Commit with button and text

---
+++ app/src/main/java/edu/cnm/deepdive/tallycounter/TallyApplication.java
@@ -0,0 +1,14 @@
+package edu.cnm.deepdive.tallycounter;
+
+import android.app.Application;
+import android.util.Log;
+
+public class TallyApplication extends Application {
+
+  @Override
+  public void onCreate(){
+    super.onCreate();
+    // TODO: 2/5/2024 App -level iniitialization goes here
+    Log.d(getClass().getSimpleName(), "TallyApplication::onCreate");
+  }
+}
