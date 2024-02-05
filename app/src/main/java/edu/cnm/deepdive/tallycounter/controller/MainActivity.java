4e858d343b5534ccc41e7f6f749376ef07428738
Reed Searle <csearle@cnm.edu>
2024-02-05 15:31:02 -0700

    Moved state to viewModel

--- app/src/main/java/edu/cnm/deepdive/tallycounter/controller/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/tallycounter/controller/MainActivity.java
@@ -8,15 +8,18 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 import edu.cnm.deepdive.tallycounter.R;
 import edu.cnm.deepdive.tallycounter.databinding.ActivityMainBinding;
+import edu.cnm.deepdive.tallycounter.viewmodel.MainViewModel;
 import org.jetbrains.annotations.NotNull;
 
 public class MainActivity extends AppCompatActivity {
 
   private static final String TAG = MainActivity.class.getSimpleName();
-  private int counter;
   private ActivityMainBinding binding;
+  private MainViewModel viewModel;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -24,68 +27,24 @@ public class MainActivity extends AppCompatActivity {
     Log.d(TAG, "MainActivity::onCreate");
     binding = ActivityMainBinding.inflate(getLayoutInflater());
     setContentView(binding.getRoot());
-    if(savedInstanceState!=null){
-      setCounter(savedInstanceState.getInt("counter",0));
-    }else{
-      setCounter(3);
-    }
-  }
-
-  @Override
-  protected void onRestart() {
-    super.onRestart();
-    Log.d(TAG, "MainActiviy::onRestart");
-  }
-
-  @Override
-  protected void onStart() {
-    super.onStart();
-    Log.d(TAG, "MainActivity::OnStart");
-  }
-
-  @Override
-  protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
-    super.onRestoreInstanceState(savedInstanceState);
-    Log.d(TAG, "MainActivity::onRestoreInstanceState");
-  }
-
-  @Override
-  protected void onResume() {
-    super.onResume();
-    Log.d(TAG, "MainActivity::OnResume");
-  }
-
-  @Override
-  protected void onPause() {
-    super.onPause();
-    Log.d(TAG, "MainActivity::OnPause");
-  }
-
-  @Override
-  protected void onStop() {
-    super.onStop();
-    Log.d(TAG, "MainActivity::OnStop");
-  }
-
-  @Override
-  protected void onSaveInstanceState(@NonNull Bundle outState) {
-    super.onSaveInstanceState(outState);
-    Log.d(TAG, "MainActivity::OnsavedInstanceState");
-    outState.putInt("counter",counter);
-  }
-
-  @Override
-  protected void onDestroy() {
-    super.onDestroy();
-    Log.d(TAG, "MainActivity::OnDestroy");
+    viewModel = new ViewModelProvider(this)
+        .get(MainViewModel.class);
+    viewModel
+        .getCounter()
+        .observe(this, new Observer<Integer>() {
+          @Override
+          public void onChanged(Integer value) {
+            setTallyDisplay(value);
+          }
+        });
   }
 
   public void handleIncrement(View v){
-    setCounter(counter + 1);
+    viewModel.incrementCounterValue();
   }
 
-  private void setCounter(int counter){
-    this.counter = counter;
+
+  private void setTallyDisplay(int counter){
     binding.tally.setText(String.valueOf(counter));
   }
 }
